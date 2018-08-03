e3b1d204ef420836a5c62874600201f65e27088c
jgramoll <jgramoll@instructure.com>
2018-08-02 21:13:35 -0600

    add coverage

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,31 @@
 const path = require('path')
 
 module.exports = {
-  entry: 'src/index.js',
+  entry: path.resolve(__dirname, 'src/index.js'),
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, 'src'),
+        ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [ 'env', ],
+            env: {
+              test: {
+                plugins: [
+                  'istanbul'
+                ]
+              }
+            }
+          }
+        }
+      }
+    ]
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js'
